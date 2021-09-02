29bd2aa2b4bc7e7f8c2a364047dba3c2811e493a
Tomas Mourad <tomas.mourad@gmail.com>
2021-09-02 16:11:07 -0300

    M6 U1

--- routes/novedades.js
+++ routes/novedades.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../models/novedadesModel');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('novedades', { isNovedades:true });
+router.get('/', async function(req, res, next) {
+  var novedades = await novedadesModel.getNovedades();
+  res.render('novedades', { isNovedades:true, novedades });
 });
 
 module.exports = router;
